4a000dc443431a3fd6bfa2e8e80bf4fa1810aa56
Howerd Oakford <howerd@inventio.co.uk>
2021-08-15 23:05:03 +0200

    Delete dasm.bat

--- dasm.bat
+++
@@ -1,8 +0,0 @@
-@echo Dis-assembling the image file .... please wait...
-rem                      start address  avoid n bytes from address  filename     output filename
-rem ndisasm.exe -b 32    -e 0x00000712     -k 0x00010000,0x00000FAD cf2019.img > cf2019.dasm
-
-rem                start address   filename     output filename
-rem ndisasm.exe -b 32 -e 0x00000F2D    cf2019.img > cf2019.dasm
-rem ndisasm.exe -b 32 -e 0x00000712   cf2019.img > cf2019.dasm
-ndisasm.exe -b 32 -e 0x00000000   cf2019.img > cf2019.dasm
